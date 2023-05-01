a14922bf4f1ea2f95195b00720e65a31b8e3f848
zakariaBAQASSE <zbaqasse51@gmail.com>
2023-05-01 13:53:28 +0100

    feat: added quiz feature

--- Assets/Scenes/Connaissance Des Animaux/Scripts/NarratorController.cs
+++ Assets/Scenes/Connaissance Des Animaux/Scripts/NarratorController.cs
@@ -12,34 +12,38 @@ namespace BrainScape
         public AudioClip nameClip;
         public AudioClip descriptionClip;
         AudioSource voiceOver;
+        GameManager gameManager;
         
         void Start()
         {   
+            gameManager = FindObjectOfType<GameManager>();
             voiceOver = GetComponent<AudioSource>();
             StartCoroutine(StartNarrator());
         }
 
         IEnumerator StartNarrator()
         {
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(2.5f);
             nameCanva.SetActive(true);
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1f);
             voiceOver.clip = nameClip;
             voiceOver.Play();
             yield return new WaitForSeconds(voiceOver.clip.length);
             voiceOver.Stop();
             nameCanva.SetActive(false);
 
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(2.5f);
 
             descriptionCanva.SetActive(true);
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1f);
             voiceOver.clip = descriptionClip;
             voiceOver.Play();
             yield return new WaitForSeconds(voiceOver.clip.length);
             voiceOver.Stop();
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(2.5f);
             descriptionCanva.SetActive(false);
+            yield return new WaitForSeconds(5f);
+            gameManager.ShowQuizCards();
         }
 
     }
