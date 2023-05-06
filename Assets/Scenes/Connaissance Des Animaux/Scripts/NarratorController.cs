d458eac96a7d7edaebcde408adc0c1ad16befa34
zakariaBAQASSE <zbaqasse51@gmail.com>
2023-05-06 21:16:55 +0100

    feat: added events to animation

--- Assets/Scenes/Connaissance Des Animaux/Scripts/NarratorController.cs
+++ Assets/Scenes/Connaissance Des Animaux/Scripts/NarratorController.cs
@@ -2,49 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace BrainScape
 {
     public class NarratorController : MonoBehaviour
     {
-        public GameObject descriptionCanva;
-        public GameObject nameCanva;
+        private string nameDesc = "ﺐﻠﻛ ﺍﺬﻫ";
+        private string generalDesc = "ْﻝﺎﻔﻃﻷﺍ ﻯﺪﻟ ِﺓَﺑَّﺒﺤﻤﻟﺍ ﺔﻔﻴﻟﻷﺍ ﺕﺎﻧﺍﻮﻴﺤﻟﺍ ﻦﻣ ﺐﻠﻜﻟﺍ";
+        private string barkingDesc = "ِﺡﺎﺑُّﻨﻟﺍ ُﺓَّﻳِﻋْﺿَﻭ ِﻩِﺫَﻫ";
+        private string attackDesc="ِﻡﻭُﺟُﻬﻟﺍ ُﺓَّﻳِﻋْﺿَﻭ ِﻩِﺫَﻫ";
+        private string fightDesc = "ِﻙﺍﺭِﻌﻟﺍ ُﺓَّﻳِﻋْﺿَﻭ ِﻩِﺫَﻫ";
+        
+
+        public TextMeshPro descriptionCanva;
         public AudioClip nameClip;
-        public AudioClip descriptionClip;
+        public AudioClip generalDescriptionClip;
+        public AudioClip barkingDescriptionClip;
+        public AudioClip attackDescriptionClip;
+        public AudioClip fightDescriptionClip;
         AudioSource voiceOver;
         GameManager gameManager;
         
         void Start()
         {   
+            descriptionCanva.isRightToLeftText = true;
             gameManager = FindObjectOfType<GameManager>();
             voiceOver = GetComponent<AudioSource>();
-            StartCoroutine(StartNarrator());
+            StartCoroutine(ShowNameCoroutine());
         }
 
-        IEnumerator StartNarrator()
-        {
+        IEnumerator ShowNameCoroutine(){
             yield return new WaitForSeconds(2.5f);
-            nameCanva.SetActive(true);
+            descriptionCanva.text = nameDesc;
             yield return new WaitForSeconds(1f);
             voiceOver.clip = nameClip;
             voiceOver.Play();
             yield return new WaitForSeconds(voiceOver.clip.length);
             voiceOver.Stop();
-            nameCanva.SetActive(false);
-
             yield return new WaitForSeconds(2.5f);
-
-            descriptionCanva.SetActive(true);
             yield return new WaitForSeconds(1f);
-            voiceOver.clip = descriptionClip;
+            descriptionCanva.text = generalDesc;
+            voiceOver.clip = generalDescriptionClip;
             voiceOver.Play();
             yield return new WaitForSeconds(voiceOver.clip.length);
             voiceOver.Stop();
-            yield return new WaitForSeconds(2.5f);
-            descriptionCanva.SetActive(false);
-            yield return new WaitForSeconds(5f);
-            gameManager.ShowQuizCards();
         }
+
+
+        IEnumerator ShowBarkingDescriptionCoroutine()
+        {
+            descriptionCanva.text = barkingDesc;
+            voiceOver.clip = barkingDescriptionClip;
+            voiceOver.Play();
+            yield return new WaitForSeconds(voiceOver.clip.length);
+            voiceOver.Stop();  
+        }
+        
+        IEnumerator ShowAttackDescriptionCoroutine()
+        {
+            descriptionCanva.text = attackDesc;
+            voiceOver.clip = attackDescriptionClip;
+            voiceOver.Play();
+            yield return new WaitForSeconds(voiceOver.clip.length);
+            voiceOver.Stop();  
+        }
+
+        IEnumerator ShowFightDescriptionCoroutine()
+        {
+            descriptionCanva.text = fightDesc;
+            voiceOver.clip = fightDescriptionClip;
+            voiceOver.Play();
+            yield return new WaitForSeconds(voiceOver.clip.length);
+            voiceOver.Stop();  
+        }
+
+        // IEnumerator StartNarrator()
+        // {
+        //     yield return new WaitForSeconds(2.5f);
+        //     nameCanva.SetActive(true);
+        //     yield return new WaitForSeconds(1f);
+        //     voiceOver.clip = nameClip;
+        //     voiceOver.Play();
+        //     yield return new WaitForSeconds(voiceOver.clip.length);
+        //     voiceOver.Stop();
+        //     nameCanva.SetActive(false);
+
+        //     yield return new WaitForSeconds(2.5f);
+
+        //     descriptionCanva.SetActive(true);
+        //     yield return new WaitForSeconds(1f);
+        //     voiceOver.clip = descriptionClip;
+        //     voiceOver.Play();
+        //     yield return new WaitForSeconds(voiceOver.clip.length);
+        //     voiceOver.Stop();
+        //     yield return new WaitForSeconds(2.5f);
+        //     descriptionCanva.SetActive(false);
+        //     yield return new WaitForSeconds(5f);
+        //     gameManager.ShowQuizCards();
+        // }
 
     }
 
